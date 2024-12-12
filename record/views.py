8309a9ef27f5e3cfaa051411d9d554eca548c220
1F10230188 <s1f102301886@iniad.org>
2024-12-12 14:54:07 +0900

    学習記録ページのエラーの修正

--- record/views.py
+++ record/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def top(request):
     data = {
         "subject1": "数学",
-        "subject-list": ["CS概論", "CS演習", "SW演習", "物理", "数学"]
+        "subject_list": ["CS概論", "CS演習", "SW演習", "物理", "数学"]
     }
     return render(request, "index.html", data)

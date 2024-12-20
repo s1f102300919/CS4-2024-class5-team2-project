35e9e318fc9e6df31c88a846561b4fe9de31d26d
1F10230188 <s1f102301886@iniad.org>
2024-12-20 21:13:33 +0900

    学習記録ページの科目選択欄の追加

--- record/urls.py
+++ record/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.top, name="top"),
     path("form", views.form, name="form"),
+    path("delete_subject/", views.delete_subject, name="delete_subject"),
     ]

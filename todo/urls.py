09fa2bb974d94b614ec88fb0100860b85eceb6f9
s1F102300098 <s1f102300098@iniad.org>
2025-01-16 00:36:06 +0900

    todo/urls.py変更

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
+    path('create/', TaskCreate.as_view(), name=task_create),
 ]

07f7dda2e3524acab5255c8e6bfac4b0d1ef0f6b
s1F102300098 <s1f102300098@iniad.org>
2025-01-16 02:15:13 +0900

    todo/urls.pyに完了タスク用のurl設定

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete, complete_task
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
     path('create/', TaskCreate.as_view(), name='task_create'),
     path('<int:pk>/edit/', TaskUpdate.as_view(), name='task_edit'), 
     path('<int:pk>/delete/', TaskDelete.as_view(), name='task_delete'),
+    path('<int:pk>/complete/', complete_task, name='task_complete'),
 ]

92ed6e777c586968d35c58decefc206b6586ff31
s1F102300098 <s1f102300098@iniad.org>
2025-01-16 01:18:17 +0900

    todo/urls.pyにTaskUpdate,todo/views.pyclass追加

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskCreate
+from .views import TaskList, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
     path('create/', TaskCreate.as_view(), name='task_create'),
+    path('<int:pk>/edit/', TaskUpdate.as_view(), name='task_edit'), 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from django.views import View
+from django.urls import reverse_lazy
+from django.forms import DateInput
 from .models import Task
 from .forms import TaskForm
 
@@ -38,3 +40,17 @@ class TaskCreate(View):
             task = form.save()
         #3タスク一覧へ遷移
         return redirect('task_list')
+    
+#1.UpdateViewで作成
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'due_date']
+    template_name = 'todo/task_edit.html'
+    #2.成功時URLを指定
+    success_url = reverse_lazy('task_list')
+ 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        #3.due_dateフィールドのウィジェットを上書き
+        form.fields['due_date'].widget = DateInput(attrs={'type': 'date'})
+        return form

ab5acbff61f51fd924ba707d3356d39fa34e110f
s1F102300098 <s1f102300098@iniad.org>
2025-01-16 00:25:57 +0900

    todo/urls.pyにTaskListの追加

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import TaskList
 
 urlpatterns = [
-    path('', views.todo, name='todo'),
+    path('', TaskList.as_view(), name='task_list'),
 ]

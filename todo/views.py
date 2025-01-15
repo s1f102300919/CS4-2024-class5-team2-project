82c71f151413feb8417d42a91f8856d4b1bfb419
s1F102300098 <s1f102300098@iniad.org>
2025-01-16 00:14:45 +0900

    todo/views.pyの編集

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 from .models import Task
 
 # Create your views here.
-def todo(request):
-    tasks = Task.objects.all()
-    print(tasks)
-    return render(request, 'todo/todo.html', {tasks: tasks})
-
-def delite(request):
-    return render(request, 'todo.delite.html')
-
 def record(request):
     return render(request, 'index.html')
 
 def timeline(request):
     return render(request, 'timeline.html')
+
+class TaskList(ListView):
+    #使用するModelを指定
+    model = Task
+    #使用するテンプレートファイルを指定
+    template_name = 'todo/task_list.html'
+    #コンテキスト名を設定
+    context_object_name = 'tasks'
+    #データの並び順を指定
+    ordering = ['-created_at', 'due_date']
